12663b0a6a61c29623a2473b887b40c7a30f1974
Liam <95653151+lla1dlaw@users.noreply.github.com>
2026-08-06 03:02:57 +0000

    add --remove-orphans flag to cleanup dead procs

--- up.sh
+++ up.sh
@@ -2,4 +2,4 @@
 
 # elevate privilages if needed
 [[ "$EUID" == 0 ]] || exec sudo -s ${BASH_SOURCE[0]} "$@"
-sudo docker compose -f docker-compose.prod.yaml up -d --build
+sudo docker compose -f docker-compose.prod.yaml up -d --build --remove-orphans
